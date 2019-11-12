4f46a6d30302f84b1674ef245255457de251348a
saqbax <noe.santiago@brokmake.com>
2019-11-11 23:30:44 -0600

    se empieza construccion de pdf

--- genera_pdf.php
+++ genera_pdf.php
@@ -1,20 +1,65 @@
 <?php
-	include('conexion.php');
+	require __DIR__.'/vendor/autoload.php';
+
+	use Spipu\Html2Pdf\Html2Pdf;
+	use Spipu\Html2Pdf\Exception\Html2PdfException;
+	use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+if(isset($_POST["generar"])){
+
+	//Recogemos el contenido de la vista
+	// ob_start();
+	// require_once 'vistaImprimir.php';
+	// $html=ob_get_clean();
+
+	// $html2pdf = new Html2Pdf();
+	// $html2pdf->writeHTML($html);
+	// $html2pdf->output();
+
+
+	/* Instanciamos la clase HTML2PDF en un objeto, mediante el constructor. 
+	Le indicamos que será vertical, (P), en tamño A4 y en español (es). */
+	// $objetoPDF = new HTML2PDF('P', 'A4', 'es');
+	
+	// /* Leemos el contenido del HTML y lo guardamos en $contenido. */
+	// $contenido = $objetoPDF->getHtmlFromPage(file_get_contents('vistaImprimir.php'));
+	
+	// /* Colocamos el contenido del archivo html en el documento pdf. */
+	// $objetoPDF->WriteHTML($contenido);
+	
+	// /* Renderizamos el documento pdf y lo enviamos, directamente, al navegador. */
+	// $objetoPDF->Output();
+
+
+	
+	/**
+	 * Html2Pdf Library - example
+	 *
+	 * HTML => PDF converter
+	 * distributed under the OSL-3.0 License
+	 *
+	 * @package   Html2pdf
+	 * @author    Laurent MINGUET <webmaster@html2pdf.fr>
+	 * @copyright 2017 Laurent MINGUET
+	 */
+	// require_once dirname(__FILE__).'/../vendor/autoload.php';
+
+	try {
+		ob_start();
+		include dirname(__FILE__).'/vistaImprimir.php';
+		$content = ob_get_clean();
+		$html2pdf = new Html2Pdf('P', 'A4', 'fr');
+		$html2pdf->writeHTML($content);
+		$html2pdf->output('example01.pdf');
+	} catch (Html2PdfException $e) {
+		$html2pdf->clean();
+		$formatter = new ExceptionFormatter($e);
+		echo $formatter->getHtmlMessage();
+	}
+
+}
 ?>
-	<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand visible-xs-block visible-sm-block" href="">Inicio</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav ">
-					<li class="nav-item active"><span class="navbar-brand" style="font-size: 14px;"><b>Bienvenido</b> <?php echo $login_session; ?></span></li>
-					<li><a href="mp_list.php">Mantenimiento de personas</a></li>
-					<li class="nav-item"><a class="nav-link" href="logout.php"> Cerrar sesión</a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
-	</div>
+<form action="" method="POST">
+    <input type="submit" value="Generar PDF" name="generar"/>
+</form>
+
