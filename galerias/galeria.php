73e4ea73d6a11c086d9e3cf69b51f177c991037e
Neo <isc.noe.s.g@gmail.com>
2020-04-21 01:11:46 -0500

    se agrega interface altas de fotos

---
+++ galerias/galeria.php
@@ -0,0 +1,412 @@
+<?php
+require '../php/conexion.php';
+require('../session.php');
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="es-ES">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Fotografias por caso</title>
+    <html lang="es-ES">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,500,600,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <style>
+        html, body {
+            margin:0;
+            padding:0;
+        }
+        
+        html {
+            font-size: 62.5%;
+        }
+
+        body {
+            font-family: 'Open Sans', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-size: 18px;
+            font-size: 1.8rem;
+            background-color: #f3f3f3;
+            color: #666;
+            background: #f2f2f2; 
+            line-height: 1.5em;            
+            padding-top: 80px; /* Relleno superior igual a la altura de la cabecera*/
+
+
+        }
+
+        header {
+            text-align: center;
+        }
+        
+        main > header {
+            background-color: #02223a;
+        }
+
+        h1 {
+            color:#003366;
+            font-size: 30px;
+            font-size:3rem;
+            -webkit-column-span:all;
+            -moz-column-span:all;
+            column-span:all;
+        }
+
+        a {
+            text-decoration: none;
+            color:#036;
+        }
+
+        #galeria {
+            margin: 1rem auto;
+            width:100%;
+            max-width:960px;
+            padding:0 20px 20px;
+            box-sizing: border-box;
+            column-count: 3;
+
+            /* Espacio entre columnas */
+            -moz-column-gap: 20px;
+            -webkit-column-gap: 20px;
+            column-gap: 20px;
+            
+            /* Filete entre columnas */
+            /*-moz-column-rule: 1px solid #bbb;
+            -webkit-column-rule: 1px solid #bbb;
+            column-rule: 1px solid #bbb;*/
+
+
+        }
+        
+        #galeria header {
+            -webkit-column-span:all;
+            column-span:all;
+        }
+
+        article {
+            background:#fff;
+            border-radius:5px;
+            border:1px solid #ccc;
+            margin:0 0 20px 0;
+            text-align: center;
+            max-width: 100%;
+            filter: opacity(70%);
+
+            /*Evitamos que se corte al cambiar de columna*/
+            break-inside: avoid;
+            page-break-inside: avoid;         
+       
+        }
+        
+        article img {
+            width:100%;
+        }
+
+        article:hover {
+            transition: .5s ease;
+            filter: opacity(100%);
+        }
+
+        figure {
+            padding:1rem;
+            box-sizing: border-box;
+        }
+
+ 
+        /* Móviles en horizontal o tablets en vertical */
+        @media (max-width: 767px) { 
+            #galeria {
+                columns:2;
+            }
+
+        }
+        
+        /* Móviles en vertical */
+
+        @media (max-width: 480px) {
+            #galeria {
+                columns: 1;
+            }
+        }
+
+
+        #main-header {
+            background: #333;
+            color: white;
+            height: 80px;
+            
+            width: 100%; /* hacemos que la cabecera ocupe el ancho completo de la página */
+            left: 0; /* Posicionamos la cabecera al lado izquierdo */
+            top: 0; /* Posicionamos la cabecera pegada arriba */
+            position: fixed; /* Hacemos que la cabecera tenga una posición fija */
+            z-index: 1;
+
+        }	
+            #main-header a {
+                color: white;
+            }
+
+        /*
+        * Logo
+        */
+        #logo-header {
+            float: left;
+            padding: 15px 0 0 20px;
+            text-decoration: none;
+        }
+            #logo-header:hover {
+                color: #0b76a6;
+            }
+            
+            #logo-header .site-img {
+                display: block;
+            }
+            
+            #logo-header .site-desc {
+                display: block;
+                font-weight: 300;
+                font-size: 0.8em;
+                color: #999;
+            }
+            
+
+        /*
+        * Navegación
+        */
+        nav {
+            float: right;
+        }
+            nav ul {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+                padding-right: 20px;
+            }
+            
+                nav ul li {
+                    display: inline-block;
+                    line-height: 80px;
+                }
+                    
+                    nav ul li a {
+                        display: block;
+                        padding: 0 10px;
+                        text-decoration: none;
+                    }
+                    
+                        nav ul li a:hover {
+                            background: #0b76a6;
+                        }
+
+        /*Estilos del modal*/
+
+        .modal {
+            display: none;
+        }
+
+        .modal:target {
+            
+            display: block;
+            position: fixed;
+            background: rgba(0,0,0,0.8);
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+        }
+
+        .modal h3 {
+            color: #fff;
+            font-size: 30px;
+            text-align: center;
+            margin: 15px 0;
+        }
+
+        .imagen {
+            width: 100%;
+            height: 50%;
+            
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .imagen a {
+            color: #fff;
+            font-size: 40px;
+            text-decoration: none;
+            margin: 0 10px;
+        }
+
+        .imagen a:nth-child(2) {
+            margin: 0;
+            height: 100%;
+            flex-shrink: 2;
+        }
+
+        .imagen img {
+            width: 500px;
+            height: 100%;
+            max-width: 100%;
+            border: 7px solid #fff;
+            box-sizing: border-box;
+        }
+
+        .cerrar {
+            display: block;
+            background: #fff;
+            width: 25px;
+            height: 25px;
+            margin: 15px auto;
+            text-align: center;
+            text-decoration: none;
+            font-size: 25px;
+            color: #000;
+            padding: 5px;
+            border-radius: 50%;
+            line-height: 25px;
+        }                
+
+    </style>
+</head>
+
+<body>
+    <input type="file"  id="fileToUpload" onchange="upload_image();" style="visibility: hidden;">
+    <main>
+        <header id="main-header">
+            <a id="logo-header">
+			<span class="site-img"><img src="../img/alesi_logo1.png" width="22%" alt="Alesi" /> </span>
+            </a> <!-- / #logo-header -->
+            
+            
+
+		<nav>
+			<ul>
+				<li><a href="#"  onclick="click_load()">Cargar Imagen </a> </li>
+                <li><a href="#"  onclick="Hola()">Regresar</a></li>
+                
+			</ul>
+        </nav><!-- / nav -->         
+
+        <div class="upload-msg"></div>
+           
+        </header>
+        <section id="galeria">
+        <header>
+
+            <?php
+                $nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
+                //consultamos los datos para el cabezero
+
+                $resultado = mysqli_query($con, "SELECT f_num_siniestro(".$nik.") AS NUM_SINIESTRO");
+                $row = mysqli_fetch_assoc($resultado);
+                $content = $row['NUM_SINIESTRO'];
+
+                echo ("<h1> Caso: ".$nik."<br> Siniestro: ".$content."</h1>");
+            ?>
+            
+        </header>
+        <?php
+        //se inicia logica para mostrar las fotos asociadas al caso
+
+        $resultado = mysqli_query($con, "SELECT * from ALESI_IMAGENES where ID_CASO  = ".$nik);        
+
+        if(mysqli_num_rows($resultado) == 0){
+            echo ("No se ha cargado ninguna imagen");
+        }else{
+            $n =0;
+            while($row = mysqli_fetch_assoc($resultado)){
+                $n++;
+                echo ('<article>
+                        <a href="#img'.$n.'">
+                            <figure>
+                                <img src="imagenes/'.$row['NOMBRE_IMAGEN'].'" />
+                                <figcaption>'.$row['NOMBRE_IMAGEN'].'</figcaption>
+                            </figure>
+                        </a>
+                    </article>
+                    
+                    <div class="modal" id="img'.$n.'">
+                    <h3>'.$row['NOMBRE_IMAGEN'].'</h3>
+                    <div class="imagen">
+                        <a href="#img'.($n-1).'">&#60;</a>
+                        <a ><img src="imagenes/'.$row['NOMBRE_IMAGEN'].'"></a>
+                        <a href="#img'.($n+1).'">></a>
+                    </div>
+                    <a class="cerrar" href="">X</a>
+                    </div>
+                ');
+            }
+        }
+
+
+        ?>
+
+
+
+        </section>
+    </main>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" ></script>    
+<script>
+    function Hola(){
+        alert("Hola mundo!");
+
+    };
+    function click_load() {
+        document.getElementById("fileToUpload").click(); //click al elemento no visible
+        
+    }
+
+	function upload_image(){//Funcion encargada de enviar el archivo via AJAX
+				
+                document.getElementById("fileToUpload").click(); //click al elemento no visible
+
+                $(".upload-msg").text('Cargando...');
+
+
+				var inputFileImage = document.getElementById("fileToUpload");
+				var file = inputFileImage.files[0];
+				var data = new FormData();				
+                data.append('id_caso',<?php echo ($nik); ?>);
+                data.append('imagen',file);
+
+                console.log( data);
+				
+				/*jQuery.each($('#fileToUpload')[0].files, function(i, file) {
+					data.append('file'+i, file);
+				});*/
+							
+				$.ajax({
+					url: "../services/guarda_img.php",        // Url to which the request is send
+					type: "POST",             // Type of request to be send, called as method
+					data: data, 			  // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+					contentType: false,       // The content type used when sending data to the server.
+					cache: false,             // To unable request pages to be cached
+					processData:false,        // To send DOMDocument or non processed data file it is set to false
+					success: function(data)   // A function to be called if request succeeds
+					{
+						$(".upload-msg").html(data);
+						window.setTimeout(function() {
+						$(".alert-dismissible").fadeTo(500, 0).slideUp(500, function(){
+						$(this).remove();
+                        location.reload(true);
+						});	}, 4000);
+					}
+				});
+
+
+				
+			}
+</script>    
+</body>
+
+</html>
--- services/guarda_img.php
+++ services/guarda_img.php
@@ -12,24 +12,96 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     $imagen_temporal = $_FILES['imagen']['tmp_name'];
     $type = $_FILES['imagen']['type'];
 
+    $target_dir = "../galerias/imagenes/";
+    $carpeta=$target_dir;
+
     #echo  ('caso '.$id_caso);
     #echo  ('nombre: '.$nombre);
 
 
 
 		try {
-			
-        //archivo temporal en binario
-        $itmp = fopen($imagen_temporal, 'r+b');
-        $imagen = fread($itmp, filesize($imagen_temporal));
-        fclose($itmp);
-        //escapando los caracteres
-        $imagen = mysqli_real_escape_string($con,$imagen);
-        
-        $respuesta =  mysqli_query($con,"insert into ALESI_IMAGENES (ID_CASO, NOMBRE_IMAGEN,   F_ALTA,    IMAGEN,    TIPO) 
-                                                             values('$id_caso', '$nombre',    SYSDATE(),  '$imagen', '$type')");
 
-        //leemos la condicion 
+
+      $target_file = $carpeta . basename($_FILES["imagen"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+      // Check if image file is a actual image or fake image
+      if(isset($_POST["submit"])) {
+          $check = getimagesize($_FILES["imagen"]["tmp_name"]);
+          if($check !== false) {
+              $errors[]= "El archivo es una imagen - " . $check["mime"] . ".";
+              $uploadOk = 1;
+          } else {
+              $errors[]= "El archivo no es una imagen.";
+              $uploadOk = 0;
+          }
+      }
+      // Check if file already exists
+      if (file_exists($target_file)) {
+          $errors[]="Lo sentimos, archivo ya existe.";
+          $uploadOk = 0;
+      }
+      // Check file size
+      if ($_FILES["imagen"]["size"] > 524288) {
+          $errors[]= "Lo sentimos, el archivo es demasiado grande.  Tamaño máximo admitido: 0.5 MB";
+          $uploadOk = 0;
+      }
+      // Allow certain file formats
+      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      && $imageFileType != "gif" ) {
+          $errors[]= "Lo sentimos, sólo archivos JPG, JPEG, PNG & GIF  son permitidos.";
+          $uploadOk = 0;
+      }
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+          $errors[]= "Lo sentimos, tu archivo no fue subido.";
+      // if everything is ok, try to upload file
+      } else {
+          if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
+            $messages[]= "El Archivo ha sido subido correctamente.";
+            
+            mysqli_autocommit($con,TRUE);
+            $respuesta =  mysqli_query($con,"insert into ALESI_IMAGENES (ID_CASO, NOMBRE_IMAGEN,   F_ALTA,        TIPO) 
+                                                             values('$id_caso', '$nombre',    SYSDATE(),   '$type')");
+            
+          
+          
+          } else {
+            $errors[]= "Lo sentimos, hubo un error subiendo el archivo.";
+          }
+      }
+      
+      if (isset($errors)){
+        ?>
+        <div class="alert alert-danger alert-dismissible col-md-5" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <strong>Error!</strong> 
+          <?php
+          foreach ($errors as $error){
+            echo"<p>$error</p>";
+          }
+          ?>
+        </div>
+        <?php
+      }
+      
+      if (isset($messages)){
+        ?>
+        <div class="alert alert-success alert-dismissible col-md-5" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <strong>Aviso!</strong> 
+          <?php
+          foreach ($messages as $message){
+            echo"<p>$message</p>";
+          }
+          ?>
+        </div>
+        <?php
+      }      
+                
+
+        /*/leemos la condicion 
         if ($respuesta){
           header("HTTP/1.1 200 Guardado Exitosamente");
           exit();
@@ -40,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
           printf ("Ocurrio un error a la hora de realizar la insercion. => %d\n",mysqli_error($con));
           exit();
 
-        }
+        }*/
         
 		} catch (Exception $e) {                       			
       header("HTTP/1.1 500  fallo");
@@ -55,3 +127,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 }
 
 ?>
+
